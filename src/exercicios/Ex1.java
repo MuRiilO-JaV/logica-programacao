11b5ee8a165cb980ee41f3c56d065fb9f82f7ea5
MuRiilO-JaV <felipemurilo6@gmail.com>
2025-02-17 15:53:51 -0300

    Alteracao exercicio1

--- src/exercicios/Ex1.java
+++ src/exercicios/Ex1.java
@@ -9,29 +9,8 @@ public class Ex1 {
         Scanner scanner = new Scanner(System.in);
 
 
-        int numero;
 
 
-        System.out.print("Digite um numero: ");
-        numero = scanner.nextInt();
-
-
-        if(numero % 2 == 0 && numero > 10 && numero < 51){
-
-
-            System.out.print("True");
-
-
-
-
-        }
-        else{
-
-
-            System.out.print("False");
-
-
-        }
 
 
     }
