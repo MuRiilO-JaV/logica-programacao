1e716aaa7375ef776b190177cc827cc9fd156357
MuRiilO-JaV <felipemurilo6@gmail.com>
2025-02-17 15:16:45 -0300

    operador logico

---
+++ src/Ex1.java
@@ -0,0 +1,39 @@
+
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        int numero;
+
+
+        System.out.print("Digite um numero: ");
+        numero = scanner.nextInt();
+
+
+        if(numero % 2 == 0 && numero > 10 && numero < 51){
+
+
+            System.out.print("True");
+
+
+
+
+        }
+        else{
+
+
+            System.out.print("False");
+
+
+        }
+
+
+    }
+
+}
